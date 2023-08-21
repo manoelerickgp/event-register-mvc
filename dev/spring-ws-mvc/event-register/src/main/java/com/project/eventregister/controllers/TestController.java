ca6ee05631d80cd8fadff51b46bc79ee9cfecef1
manoelerickgp <manoel.computacao@gmail.com>
2023-08-21 00:46:50 -0300

    First commit test

---
+++ dev/spring-ws-mvc/event-register/src/main/java/com/project/eventregister/controllers/TestController.java
@@ -0,0 +1,13 @@
+package com.project.eventregister.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+    @RequestMapping("/")
+    public String test(){
+        return "index";
+    }
+}
