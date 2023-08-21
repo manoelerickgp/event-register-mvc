5766240b650c60c52d7ddd25d9c7e2a0a9e18426
manoelerickgp <manoel.computacao@gmail.com>
2023-08-21 01:15:44 -0300

    Adding Event on model

---
+++ dev/spring-ws-mvc/event-register/src/main/java/com/project/eventregister/models/Event.java
@@ -0,0 +1,51 @@
+package com.project.eventregister.models;
+
+public class Event {
+
+    private String name;
+    private String local;
+    private String date;
+    private String time;
+
+    public Event() {
+    }
+
+    public Event(String name, String local, String date, String time) {
+        this.name = name;
+        this.local = local;
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
